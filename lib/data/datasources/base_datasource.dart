2e389c56fbe718c30f3b8415924889a7e76bfeb4
eww0l <arellanosamir0@gmail.com>
2026-06-07 00:14:55 -0500

    ahdfh

--- lib/data/datasources/base_datasource.dart
+++ lib/data/datasources/base_datasource.dart
@@ -2,8 +2,8 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 
 abstract class BaseDatasource {
   // ⚙️ Dirección IP o URL unificada para que sea transportable al instante
-  // final String baseUrl = "https://saga-api-546y.onrender.com/api";
-  final String baseUrl = "http://192.168.1.18:8000/api";
+  final String baseUrl = "https://saga-api-546y.onrender.com/api";
+  //final String baseUrl = "http://192.168.1.18:8000/api";
 
   // 🛠️ FUNCIÓN AUXILIAR GENERAL: Verifica si el dispositivo tiene internet real
   Future<bool> verificarInternet() async {
